95342e9be638ad9eb686fdcf3711d39b95ab1f71
ThibautDC <thibaut.delchiappo@ynov.com>
2021-03-31 15:47:38 +0200

    2/3 ptit truc
    ajout classement.php

---
+++ @homeAGame/classement.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr"> 
+	<head >
+		<meta charset="utf-8" />
+        <title>Classement </title>
+        <link rel="stylesheet" href="./Assets/CSS/Accueil.css"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
+    </head>
+    <body>
+        <header>
+            <h1 class="title">Classement @ Home A Game </h1>
+            <div class="topnav">
+                <a class="active" href="Accueil_connecté.php">Home</a>
+                <a href="about.html">A Propos</a>
+                <a href="mission.php">Mission</a>
+                <a href="profil.php">Mon Compte</a>
+            </div>
+        </header>
+        <br/>
+		<br/>
+        <a class="lien">
+            <p> 
+                <ul>
+                    <li> 1er : </li>
+                    <li> 2eme : </li>
+                    <li> 3eme : </li>
+                    <li> 4eme : </li>
+                    <li> 5eme : </li>
+                    <li> XXX : </li>
+                </ul>
+            </p> 
+        </a>
+
+    </body>
+</html>
