82b574dbbe9d3e438a40d736a7a43e7be05449c9
Theo-Bhang <theo.bhang@ynov.com>
2021-05-04 14:03:57 +0200

    error 404

---
+++ @homeAGame/404.php
@@ -0,0 +1,34 @@
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <link rel="stylesheet" href="./Assets\CSS/style2.css"/>
+  <title>404</title>
+</head>
+<header>
+        <img class="logo" src="./Img/LOGO-ON-THE-ROAD.png" alt="logo">
+        <nav>
+            <ul class="nav_links">
+                <li><a href="./Accueil/accueil.php">Accueil</a></li>
+                <li><a href="./OTRA/classement.php">Classement</a></li>
+                <li><a href="./Divers/about.php">A propos</a></li>   
+            </ul>
+        </nav>
+        <a class="cta" href="./Log/connexion.php"><button>Se connecter</button></a>
+    </header>
+
+<body>
+<h1>404</h1>
+  <div>
+    <p>La page que vous recherchez n'est pas disponible.</p> 
+  </div>
+           
+  <div>
+    <a href="./Accueil/accueil.php">Retour à la page d'accueil</a>
+  </div>
+        
+  </div>
+</body>
+
+</html>
